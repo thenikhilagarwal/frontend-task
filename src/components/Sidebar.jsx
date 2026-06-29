f923e286892652f9604756b754f04ee364940938
Nikhil Agarwal <51751244+thenikhilagarwal@users.noreply.github.com>
2026-06-29 18:31:21 +0530

    Implement responsive sidebar collapse feature
    Added responsive sidebar collapse functionality based on window size. Updated class names and button interactions for better user experience.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,42 @@
 import { Sun, Moon, PanelLeftClose } from 'lucide-react';
 import logo from '../assets/logo.svg';
+import { useEffect, useState } from 'react';
 
 export default function Sidebar({ theme, setTheme, activeTab, setActiveTab }) {
+
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+  const handleResize = () =>{
+    if(window.innerWidth < 768){
+      setIsSidebarCollapsed(true)
+    }else{
+      setIsSidebarCollapsed(false)
+    }
+  }
+
+  useEffect(()=>{
+    handleResize(); // Run on initial load
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+
+  },[])
+
+  
+
+
   return (
-    <aside className="w-64 border-r border-slate-100 dark:border-slate-800 bg-white dark:bg-slate-900 flex flex-col justify-between h-screen sticky top-0 transition-colors duration-200 select-none">
+    <aside className={`relative border-r border-slate-100 dark:border-slate-800 bg-white dark:bg-slate-900 flex flex-col justify-between h-screen sticky top-0 transition-all duration-300 select-none ${isSidebarCollapsed ? 'activeSidebar w-15' : 'w-64 '}`}>
       {/* Top Brand Header */}
       <div>
         <div className="flex items-center justify-between px-4 py-5">
           <div className="flex items-center gap-3">
             <img src={logo} alt="logo" />
           </div>
-          <button className="text-slate-400 hover:text-slate-600 dark:hover:text-slate-200 cursor-pointer p-1 rounded-md hover:bg-slate-50 dark:hover:bg-slate-800 transition-colors">
+          <button className={`text-slate-400 hover:text-slate-600 dark:hover:text-slate-200 cursor-pointer p-1 rounded-md hover:bg-slate-50 dark:hover:bg-slate-800 transition-colors`} onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}>
             <PanelLeftClose className='size-5' />
           </button>
         </div>
@@ -19,7 +45,7 @@ export default function Sidebar({ theme, setTheme, activeTab, setActiveTab }) {
         <nav className="p-4 space-y-1">
           <button
             onClick={() => setActiveTab('campaign')}
-            className={`w-full flex items-center gap-3 px-4 py-3 rounded-sm font-medium text-sm transition-all duration-150 cursor-pointer ${activeTab === 'campaign'
+            className={`w-full flex items-center gap-3 ${isSidebarCollapsed ? 'justify-center' : 'px-4'} py-3 rounded-sm font-medium text-sm transition-all duration-150 cursor-pointer ${activeTab === 'campaign'
               ? 'bg-linear-to-r from-grideant-2 from-30% to-grideant-1 via-100% hover:from-grideant-2 hover:to-grideant-2 text-white shadow-md shadow-blue-500/10 transition-all duration-200'
               : 'text-slate-500 dark:text-slate-400 hover:bg-slate-50 dark:hover:bg-slate-800/60 hover:text-slate-800 dark:hover:text-slate-200'
               }`}
@@ -29,13 +55,23 @@ export default function Sidebar({ theme, setTheme, activeTab, setActiveTab }) {
               <path d="M13.5 1.5C11.1727 1.5 6.34884 3.28321 3.57829 4.39029C2.30956 4.89726 1.5 6.13374 1.5 7.5C1.5 8.86627 2.30956 10.1027 3.57829 10.6097C6.34884 11.7168 11.1727 13.5 13.5 13.5" stroke="#F6F6F6" strokeWidth="1.125" />
               <path d="M8.25 16.5L6.79255 15.6977C5.20573 14.8243 4.3099 13.06 4.5341 11.25" stroke="#F6F6F6" strokeWidth="1.125" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
-            <span>Campaign</span>
+            <span className={isSidebarCollapsed ? 'hidden' : ''}>Campaign</span>
           </button>
         </nav>
       </div>
 
       {/* Bottom Profile and Settings */}
       <div className="p-4 space-y-4">
+        {isSidebarCollapsed ? 
+        <div className="relative">
+          <img
+            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt="John Doe"
+            className="size-8 rounded-full object-cover border border-slate-200 dark:border-slate-700"
+          />
+          <span className="absolute bottom-0 right-0 w-2.5 h-2.5 bg-emerald-500 border-2 border-slate-50 dark:border-slate-900 rounded-full"></span>
+        </div>
+        : <>
         <div className='bg-gray dark:bg-slate-800/40 p-3 rounded-lg'>
           {/* User Card */}
           <div className="flex items-center justify-between mb-3">
@@ -75,7 +111,6 @@ export default function Sidebar({ theme, setTheme, activeTab, setActiveTab }) {
             </div>
           </div>
         </div>
-
         {/* Theme Toggle Widget */}
         <div className="flex p-1 bg-slate-100 dark:bg-slate-800 rounded-full border border-slate-100/50 dark:border-slate-800/50">
           <button
@@ -99,6 +134,11 @@ export default function Sidebar({ theme, setTheme, activeTab, setActiveTab }) {
             <span>Dark</span>
           </button>
         </div>
+        </>
+        }
+        
+
+        
       </div>
     </aside>
   );
